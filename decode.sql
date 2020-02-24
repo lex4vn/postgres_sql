e909ffb27396f18091656d4b6191dcc756d30192
Lex <lex4vn@gmail.com>
2020-02-24 14:11:55 +0700

    Update decode.sql

--- decode.sql
+++ decode.sql
@@ -1,1 +1,9 @@
 select decode('YmFzZTY0IGVuY29kZWQgc3RyaW5n','base64')
+
+SELECT CONVERT_FROM(DECODE('SXQncyBsdW5jaCB0aW1l', 'BASE64'), 'UTF-8');
+
+-- Decoding
+SELECT CONVERT_FROM(DECODE(field, 'BASE64'), 'UTF-8') FROM table;
+
+-- Encoding
+SELECT ENCODE(CONVERT_TO(field, 'UTF-8'), 'base64') FROM table;
